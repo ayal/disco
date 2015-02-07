a33453a77ffe114ab15c5deda241ebde1de01683
ayal <ayalgelles@gmail.com>
2015-02-07 12:54:52 +0200

    disco

--- disco.js
+++ disco.js
@@ -27,12 +27,12 @@ var play = function (interval) {
     setInterval(function(){
 	    if (new Date - lasttime >= interval) {
 		if (queue.length) {
+		    vca.gain.value = 0;
 		    vco.frequency.value = queue[0];	
 		    vca.gain.value = 1;
 		    queue = queue.splice(1);
 		    lasttime = new Date();
 		}	 
-		vca.gain.value = 0;
 	    }   
 	}, 10);
 };
