560999f1a0c58c5b0cb78a8591b28bbeda9b2938
ayal <ayalgelles@gmail.com>
2015-02-09 11:08:27 +0200

    alients

--- disco.js
+++ disco.js
@@ -40,39 +40,16 @@ var Voice = (function(context) {
 		},1000 + t * 1000);
 	};
 
-
-	Voice.prototype.start = function() {
-	    var rgbav = 'rgba(' + this.frequency / 2.5 + ',0,' + this.frequency / 2.5 + ',' + 1 + ')';
-	    $('body').css('background-color', rgbav)
-
-	    /* VCO */
-	    this.vco.type = vco.SINE;
-	    this.vco.frequency.value = this.frequency;
-
-	    this.vca.gain.value = 1;
-
-	    /* connections */
-	    this.vco.connect(this.vca);
-	    this.vca.connect(context.destination);
-	    
-	    try {
-		this.vco.start(0);
-	    }
-	    catch (ex) {
-	    }
-	};
-
 	return Voice;
     })(context);
 
 
-makesound = function(buffer, ff) {
+makesound = function(buffer) {
     var source = context.createBufferSource();
     source.buffer = buffer;
-    //    source.connect(context.destination);
     var gainNode = context.createGain();
     gainNode.gain.value = 0.5; // reduce volume by 1/4
-    source.playbackRate.value = rnd(5,50) / 10 ;
+    //source.playbackRate.value = rnd(5,50) / 10 ;
     source.connect(gainNode);
     gainNode.connect(context.destination);
 
@@ -85,11 +62,11 @@ var rnd = function(a,b) {
 
 function finishedLoading(bufferList) {
     var startTime = context.currentTime + 0.100;
-    var tempo = 140; // BPM (beats per minute)
+    var tempo = 160; // BPM (beats per minute)
     var bps = 60 / tempo;
     var eighthNoteTime = (60 / tempo) / 2;
 
-    for (var h = 0; h < 8; h++) {
+    for (var h = 0; h < 2; h++) {
 	for (var j = 0; j < 8; ++j) {
 	    for (var i = 0; i < 16; ++i) {
 		var f;
@@ -97,19 +74,18 @@ function finishedLoading(bufferList) {
 
 		var time = startTime + (h * 8 * 16 + j * 16 + i) * bps * 0.25;
 		var v = new Voice(f);
-		h > 1 && v.startx(time,  0.05);
-		if (i === 0 ||  i === 10) {
-		    var s = makesound(bufferList[1], 400);
-		    s.start(time);
-		    s.stop(time + 0.1)
+		h > 0 && v.startx(time,  0.05);
+		if (i === 0 || i === 2 || i === 10) {
+		    makesound(bufferList[1]).start(time);
 		}
 
-		if (i === 4 || i === 12) {
-		    makesound(bufferList[2]).start(time);
+		if (i === 4 || i === 12 || i === 7 || i === 9) {
+		    makesound(bufferList[0]).start(time);
 		}
 
-		if (i === 5 || i === 11 ) {
-		    makesound(bufferList[3]).start(time);
+		if ([0,2,4,6,7,8,9,10,12,14].indexOf(i) !== -1) {
+		    var hh = makesound(bufferList[2]);
+		    hh.start(time);
 		}
 
 	    }
