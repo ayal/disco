a6e4ba5c796602df85e0442ca67cce4af2999c9d
ayal <ayalgelles@gmail.com>
2015-05-16 09:44:57 +0300

    amen

--- amen.js
+++ amen.js
@@ -207,33 +207,33 @@ var gen = _.debounce(function(okgo) {
 
 		    // R
 		    if (i % 2 === 0) {
-			makesound(bufferList[0], rnd(1,2)/3, 0.02, time(i+4), time(i));
+			makesound(bufferList[0], rnd(1,3)/2, 0.05, time(i+4), time(i));
 		    }
 
 		    // snare
 		    if ([4,7,9,12,15].indexOf(i) !== -1 && [0,1].indexOf(j) !== -1) {
-			makesound(bufferList[1], rnd(1,2)/3, 0.05, time(i+1), time(i));
+			makesound(bufferList[1], rnd(1,3)/2, 0.07, time(i+1), time(i));
 		    }
 
 		    if ([4,7,9,14].indexOf(i) !== -1 && [2].indexOf(j) !== -1) {
-			makesound(bufferList[1], rnd(1,3)/3, 0.05, time(i+1), time(i));
+			makesound(bufferList[1], rnd(1,3)/2, 0.07, time(i+1), time(i));
 		    }
 
 		    if ([1,4,7,9,14].indexOf(i) !== -1 && [3].indexOf(j) !== -1) {
-			makesound(bufferList[1], rnd(1,2)/3, 0.05, time(i+1), time(i));
+			makesound(bufferList[1], rnd(1,3)/2, 0.07, time(i+1), time(i));
 		    }
 
 		    // bass
 		    if ([0,2,10,11].indexOf(i) !== -1 && [0,1].indexOf(j) !== -1) {
-			makesound(bufferList[2], rnd(1,2)/3, 0.06, time(i+1), time(i));
+			makesound(bufferList[2], rnd(1,3)/2, 0.08, time(i+1), time(i));
 		    }
 
 		    if ([0,2,10].indexOf(i) !== -1 && [2].indexOf(j) !== -1) {
-			makesound(bufferList[2], rnd(1,2)/3, 0.06, time(i+1), time(i));
+			makesound(bufferList[2], rnd(1,3)/2, 0.08, time(i+1), time(i));
 		    }
 
 		    if ([2,3,10].indexOf(i) !== -1 && [3].indexOf(j) !== -1) {
-			makesound(bufferList[2], rnd(1,2)/3, 0.06, time(i+1), time(i));
+			makesound(bufferList[2], rnd(1,3)/2, 0.08, time(i+1), time(i));
 		    }
 
 
